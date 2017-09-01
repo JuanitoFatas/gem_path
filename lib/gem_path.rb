b530ad0b0585b56059a3ec45405d7140da8c8a97
Juanito Fatas <juanito-fatas@cookpad.jp>
2017-09-01 17:14:47 +0200

    Implement GemPath

--- lib/gem_path.rb
+++ lib/gem_path.rb
@@ -1,4 +1,32 @@
 require "gem_path/version"
 
-module GemPath
+class GemPath
+  GemNotFound = Class.new(RuntimeError)
+
+  def self.find(name)
+    new(name).find
+  end
+
+  def initialize(name)
+    @name = name
+  end
+
+  def find
+    if gem_exists?
+      gemspec.gem_dir
+    else
+      raise GemNotFound, "Couldn't find gem directory for '#{name}'"
+    end
+  end
+
+  private
+  attr_reader :name
+
+  def gem_exists?
+    !Gem::Specification.find_all_by_name(name).empty?
+  end
+
+  def gemspec
+    @_gemspec ||= Gem::Specification.find_by_name(name)
+  end
 end
--- spec/gem_path_spec.rb
+++ spec/gem_path_spec.rb
@@ -4,4 +4,15 @@ RSpec.describe GemPath do
   it "has a version number" do
     expect(GemPath::VERSION).not_to be nil
   end
+
+  it "raises GemNotFound error when gem cannot be found" do
+    expect { GemPath.find("notexists") }.to raise_error GemPath::GemNotFound
+  end
+
+  it "return directory of given gem" do
+    result = GemPath.find("gem_path")
+
+    expect(File.directory?(result)).to be true
+    expect(result).to match %r(/gem_path)
+  end
 end
